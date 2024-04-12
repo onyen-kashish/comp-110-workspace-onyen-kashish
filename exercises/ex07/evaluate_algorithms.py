374b9dcb075edd205ccf788bb5f1368ea8d7bce2
MadSwa <xml.j2ee@yahoo.com>
2024-04-11 22:09:54 -0400

    EX07 - Runtime Analysis

--- exercises/ex07/evaluate_algorithms.py
+++ exercises/ex07/evaluate_algorithms.py
@@ -8,16 +8,16 @@ from exercises.ex07.runtime_analysis_functions import evaluate_runtime, evaluate
 START_SIZE: int = 0
 END_SIZE: int = 1000
 
-# times = evaluate_runtime("selection_sort", START_SIZE, END_SIZE)
-# plt.plot(times)
-# plt.title("Runtime Analysis of Selection Sort - kashish")
-# plt.show()
+times = evaluate_runtime("selection_sort", START_SIZE, END_SIZE)
+plt.plot(times)
+plt.title("Runtime Analysis of Selection Sort - kashish")
+plt.show()
 
-# times = evaluate_runtime("insertion_sort", START_SIZE, END_SIZE)
-# plt.plot(times)
-# plt.title("Runtime Analysis of Insertion Sort - kashish")
-# plt.show()
-# #
+times = evaluate_runtime("insertion_sort", START_SIZE, END_SIZE)
+plt.plot(times)
+plt.title("Runtime Analysis of Insertion Sort - kashish")
+plt.show()
+#
 usage = evaluate_memory_usage("selection_sort", START_SIZE, END_SIZE)
 plt.plot(usage)
 plt.title("Memory Usage Analysis of Selection Sort - kashish")
